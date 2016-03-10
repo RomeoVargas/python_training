eb2448d6a7a52a959db708b6aa47acaa08ebc3cc
RomeoVargas <r.vargas@klab.com>
2016-03-10 16:49:10 +0800

    Updated list2 exercise

--- python_training/list2.py
+++ python_training/list2.py
@@ -14,7 +14,7 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-    return sorted(list1 + list2) 
+    return sorted([x for x in list1] + [y for y in list2]) 
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
